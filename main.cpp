a13254856e0996c41e76fdbde273aef5991417b0
junglesub <42944002+junglesub@users.noreply.github.com>
2021-01-16 02:07:19 +0900

    Squashed commit of the following:
    commit f68206d0ceae6f34a202a6161211fde99e62fd2d
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Sat Jan 16 02:05:45 2021 +0900
    
        Final before merge
    
    commit 9388dad1e80e1e250916fb8239c6f17c71d588aa
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Sat Jan 16 01:46:06 2021 +0900
    
        Created Matrix
    
    commit cd3de4ae824323d58833177e0dfb2adffbb3149e
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Sat Jan 16 01:38:54 2021 +0900
    
        With updated Prev Values
    
    commit e559891398e375710aa96decc0c4e5bed70235d9
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Sat Jan 16 01:36:26 2021 +0900
    
        Ready to remove
    
    commit 12ee810c8662de727459e5819333cc70e4bf950b
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Fri Jan 15 23:57:13 2021 +0900
    
        Remove Keyboard Arrow keys from controls
    
    commit 7d627740248198a6f33d69ab9718a42f5d740a01
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Fri Jan 15 23:54:24 2021 +0900
    
        블럭 움직이게 만듦
    
    commit 209c546a8e18a60a7ebf0bd77a34a5cd22db670d
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Fri Jan 15 23:30:00 2021 +0900
    
        independent scoreboard area
    
    commit a9a34adb2147442f06440110f5b8ace1d1a828e6
    Author: junglesub <42944002+junglesub@users.noreply.github.com>
    Date:   Fri Jan 15 23:09:11 2021 +0900
    
        틀 만들기 완료

---
+++ main.cpp
@@ -0,0 +1,172 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include "Console.h"
+#include "block.hpp"
+
+using namespace std;
+
+void drawMainFrame(void);
+void drawScoreBoard(void);
+void showBlock(Block block, string s = "#");
+void removeBlock(Block block, string s = " ");
+
+int const screenWidth = getWindowWidth();
+int const screenHeight = getWindowHeight() - 3;
+string const BORDERCH = "▓";
+
+int main(void) {
+  // Console Setting
+
+  EnableCursor(0);
+  clrscr();
+
+  drawMainFrame();
+  drawScoreBoard();
+
+  int t = 0;
+  char ch = NULL;
+
+  // 2차원 동적 Matrix [x][y] 으로 이용.
+  int **matrix = new int *[screenWidth / 3 * 2];
+  for (int col = 0; col < screenWidth / 3 * 2; col++) {
+    matrix[col] = new int[screenHeight];
+  }
+
+  Block &block = *newBlock(3, 5);
+
+  while (1) {
+    // 키보드 입력
+    if (kbhit()) {
+      ch = getch();
+
+      if (ch == 27 || ch == 'q') {
+        break;
+      } else if (ch == 'a') {
+        block.left();
+      } else if (ch == 'd') {
+        block.right();
+      }
+
+      ch = NULL;
+    }
+
+    if (t % 100 == 0) {
+      block.down();
+    }
+
+    // Finally Display the Block
+    // Remove Last saved Block
+    if (t > 0) {
+      removeBlock(block);
+    }
+
+    // Display block
+    showBlock(block);  // showBlock(block, "#");
+
+    t++;
+    gotoxy(1, screenHeight + 1);
+    MySleep(10);
+  }
+  gotoxy(1, 1);
+  printf("Good bye!\n");
+}
+
+void showBlock(Block block, string s) {
+  for (int x = 0; x < 4; x++) {
+    for (int y = 0; y < 4; y++) {
+      if (block.shape[x][y] == 2) {
+        gotoxy(block.x + x, block.y + y);
+        cout << s;
+      }
+    }
+  }
+}
+void removeBlock(Block block, string s) {
+  for (int x = 0; x < 4; x++) {
+    for (int y = 0; y < 4; y++) {
+      if (block.pre_shape[x][y] == 2) {
+        gotoxy(block.pre_x + x, block.pre_y + y);
+        cout << s;
+      }
+    }
+  }
+}
+
+void drawMainFrame(void) {
+  // https://jrgraphix.net/r/Unicode/2500-257F
+  // 여기 참고.
+
+  clrscr();      // 화면 지우기
+  gotoxy(0, 0);  // 좌측 상단에서 시작.
+
+  // 맨 위 가로 # 그리기
+  for (int x = 0; x < screenWidth; x++) {
+    gotoxy(x, 0);
+    cout << "═";
+  }
+
+  // 맨 위 모서리
+  gotoxy(0, 0);  // 왼쪽
+  cout << "🥕";
+  // cout << "╔";
+  gotoxy(screenWidth - 1, 0);  // 오른쪽
+  cout << "🥕";
+  // cout << "╗";
+
+  // 테두리 만들기
+  for (int y = 2; y < screenHeight; y++) {
+    gotoxy(1, y);
+    cout << "║";
+    for (int x = 2; x < screenWidth - 1; x++) {
+      gotoxy(x, y);
+      cout << " ";
+    }
+    cout << "║";
+  }
+
+  // 맨 아래 # 그리기
+  for (int x = 0; x < screenWidth; x++) {
+    gotoxy(x, screenHeight);
+    cout << "═";
+  }
+
+  // 맨 아래 모서리
+  gotoxy(0, screenHeight);  // 왼쪽
+  // cout << "╚";
+  cout << "🥕";
+  gotoxy(screenWidth - 1, screenHeight);  // 오른쪽
+  // cout << "╝";
+  cout << "🥕";
+}
+
+void drawScoreBoard(void) {
+  int const scoreboardSize = screenWidth / 3;
+  int const scoreboardStart = scoreboardSize * 2;
+  // 왼쪽 오른쪽 나누기
+  for (int y = 0; y < screenHeight; y++) {
+    gotoxy(scoreboardStart, y);
+    cout << "║";
+
+    for (int x = 2; x < scoreboardSize - 2; x++) {
+      if (y < 2 || y > screenHeight - 1) continue;
+      // 빈공간으로 바꾸기
+      gotoxy(scoreboardStart + x, y);
+      cout << " ";
+    }
+  }
+  // 모서리
+  gotoxy(scoreboardStart, 0);  // 위
+  cout << "╦";
+  gotoxy(scoreboardStart, screenHeight);  // 아래
+  cout << "╩";
+
+  // 기본적인 텍스트 입력
+  gotoxy(scoreboardStart + scoreboardSize / 2 - 9, 2);
+  cout << "소은정섭의 테트리스";
+  // for (int x = 1; x < scoreboardSize - 2; x += 2) {
+  //   gotoxy(scoreboardStart + x, 3);
+  //   cout << "🥕";
+  // }
+}
