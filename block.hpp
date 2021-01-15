703bf56386f03fadddd9379ea232bc767abb9548
So-Eun Yu <so-eunyu@SoEunuicBookPro.T-mobile.com>
2021-01-16 01:43:00 +0900

    add rotate fuction's parameter
    ------- Detail -------
    
    void rotate(int direction);

--- block.hpp
+++ block.hpp
@@ -4,15 +4,15 @@ struct Block
     int pre_x, pre_y;
     
     int rotation, block_type;
-    int ** shape;            // shape[x][y]
-    int ** pre_shape;            // shape[x][y]
+    int ** shape;                   // shape[x][y]
+    int ** pre_shape;               // shape[x][y]
 
-    void down();                // 블럭 수정
-    void left();                // 왼쪽, 오른쪽
-    void right();               // 오른쪽
-    void rotate();              // 회전
+    void down();                    // 블럭 수정
+    void left();                    // 왼쪽, 오른쪽
+    void right();                   // 오른쪽
+    void rotate(int direction);     // 회전
 };
 
-Block * newBlock(int &x, int &y);         // 블럭 데이터 생성, shape 4 * 4, rotate, 초깃값 내가
-void delBlock(Block &delBlock);                 // 블럭 데이터 삭제
+Block * newBlock(int &x, int &y);                       // 블럭 데이터 생성, shape 4 * 4, rotate, 초깃값 내가
+void delBlock(Block * delBlock);                        // 블럭 데이터 삭제
 void setShape(int b_type, int ** shape, int rotation);  // 블럭타입 set
