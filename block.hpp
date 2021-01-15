f6344391bbf35818b9ed263c3e929ebbda8cca59
So-Eun Yu <so-eunyu@SoEunuicBookPro.T-mobile.com>
2021-01-15 22:32:29 +0900

    block.hpp shape 변수 추가

--- block.hpp
+++ block.hpp
@@ -1,15 +1,14 @@
-#include "Console.h"
-
 struct Block 
 {
     int rotation, x, y;
+    int shape[1][4][4];         // shape[rotation][x][y]
 
     void del();                 // 블럭 데이터 삭제
 
-    void down();               // 블럭 수정
-    void left();               // 왼쪽, 오른쪽
-    void right();              // 오른쪽
-    void rotate();             // 회전
+    void down();                // 블럭 수정
+    void left();                // 왼쪽, 오른쪽
+    void right();               // 오른쪽
+    void rotate();              // 회전
 };
 
 Block * newBlock(int &x, int &y);         // 블럭 데이터 생성, shape 4 * 4, rotate, 초깃값 내가
