9a528a02e7a4c66a61abc50c84973cea1fd892b5
So-Eun Yu <so-eunyu@SoEunuicBookPro.T-mobile.com>
2021-01-15 23:40:14 +0900

    delBlock 함수 파라미터 추가 및 위치 이동
    ----- 세부 사항 -----
    
    void delBlock(Block * delBlock);

--- block.hpp
+++ block.hpp
@@ -1,10 +1,8 @@
 struct Block 
 {
-    int x, y;
+    int x, y; 
     int rotation, block_type;
     int ** shape;            // shape[x][y]
-
-    void del();                 // 블럭 데이터 삭제
 
     void down();                // 블럭 수정
     void left();                // 왼쪽, 오른쪽
@@ -13,4 +11,5 @@ struct Block
 };
 
 Block * newBlock(int &x, int &y);         // 블럭 데이터 생성, shape 4 * 4, rotate, 초깃값 내가
+void delBlock(Block * delBlock);                 // 블럭 데이터 삭제
 void setShape(int b_type, int ** shape, int rotation);  // 블럭타입 set
