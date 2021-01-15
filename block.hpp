a69d33948a25baa1e1f94862c3096b04580a4184
So-Eun Yu <so-eunyu@SoEunuicBookPro.T-mobile.com>
2021-01-15 18:04:36 +0900

    block.hpp 생성

---
+++ block.hpp
@@ -0,0 +1,13 @@
+struct Block 
+{
+    int rotate, x, y;
+
+    void del();                 // 블럭 데이터 삭제
+
+    void down();               // 블럭 수정
+    void left();               // 왼쪽, 오른쪽
+    void right();              // 오른쪽
+    void rotate();             // 회전
+};
+
+Block * newBlock(int &x, int &y)         // 블럭 데이터 생성, shape 4 * 4, rotate, 초깃값 내가
